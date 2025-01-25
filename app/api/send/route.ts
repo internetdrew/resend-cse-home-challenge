dcf847dd814804e13e81c9d78d5a1e28cb99f61a
Andrew Rowley <andrew@internetdrew.com>
2025-01-25 18:30:49 -0500

    add error handling for no firstName

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,15 +1,21 @@
-import FailedBillingTemplate from '@emails/FailedBillingEmail';
+import FailedBillingTemplate from '@/emails/FailedBillingEmail';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(request: Request) {
   try {
+    const { firstName } = await request.json();
+    
+    if (!firstName) {
+      return Response.json({ error: 'First name is required' }, { status: 400 });
+    }
+
     const { data, error } = await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: ['andrew@internetdrew.com'],
       subject: 'Looks like your payment failed',
-      react: FailedBillingTemplate({ firstName: 'Jonnie' }),
+      react: FailedBillingTemplate({ firstName }),
     });
 
     if (error) {
