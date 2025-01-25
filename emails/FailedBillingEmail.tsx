e5a01c098701132d77180f85a31771117ad6074c
Andrew Rowley <andrew@internetdrew.com>
2025-01-25 18:30:07 -0500

    move email templates directory

---
+++ emails/FailedBillingEmail.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Html, Button, Head, Body } from '@react-email/components';
+
+type FailedBillingTemplateProps = {
+  readonly firstName: string;
+};
+
+const FailedBillingTemplate = ({
+  firstName = 'Customer',
+}: FailedBillingTemplateProps) => {
+  return (
+    <Html lang='en'>
+      <Head>
+        <title>Failed Billing</title>
+      </Head>
+      <Body>
+        <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>
+          {firstName}, it looks like your payment failed
+        </h1>
+        <p>
+          We&apos;ve noticed that your payment has failed. Please update your
+          payment information and try again.
+        </p>
+        <p>
+          This email was sent from{' '}
+          <strong>Andrew&apos;s Resend CSE Home Challenge</strong>.
+        </p>
+        <p>Check out the repo here:</p>
+        <Button
+          href='https://github.com/internetdrew/resend-cse-home-challenge'
+          style={{
+            color: '#fff',
+            padding: '10px 20px',
+            backgroundColor: '#000',
+            borderRadius: '4px',
+            textDecoration: 'none',
+          }}
+        >
+          View the repo
+        </Button>
+      </Body>
+    </Html>
+  );
+};
+
+export default FailedBillingTemplate;
