2ccbc71d075314a4fdaae9e0d9a8be7e55bc95a2
Andrew Rowley <andrew@internetdrew.com>
2025-01-27 09:25:48 -0500

    update email template to use more react email components

--- emails/FailedBillingEmail.tsx
+++ emails/FailedBillingEmail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Html, Head, Body } from '@react-email/components';
+import { Html, Head, Body, Text, Link } from '@react-email/components';
 
 type FailedBillingTemplateProps = {
   readonly firstName: string;
@@ -14,42 +14,57 @@ const FailedBillingTemplate = ({
         <title>Failed Billing</title>
       </Head>
       <Body>
-        <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>
+        <Text style={{ fontSize: '20px', fontWeight: 'bold' }}>
           {firstName}, it looks like your payment failed
-        </h1>
-        <p>
+        </Text>
+        <Text>
           We&apos;ve noticed that your payment has failed. Please update your
           payment information and try again.
-        </p>
-        <p>
+        </Text>
+        <Text>
           This email was sent from{' '}
           <strong>Andrew&apos;s Resend CSE Home Challenge</strong>.
-        </p>
-        <p>
+        </Text>
+        <Text>
           Included in this email is a link to the Resend CSE Home Challenge
           tutorial repo, my responses to customer tickets, and a Loom of a bug I
           found while going through this process.
-        </p>
-        <p>
+        </Text>
+        <Text>
           If you have any questions, please contact me at{' '}
-          <a href='mailto:andrew@internetdrew.com'>andrew@internetdrew.com</a>.
-        </p>
-        <p>Here are some links to help you:</p>
+          <Link href='mailto:andrew@internetdrew.com'>
+            andrew@internetdrew.com
+          </Link>
+          .
+        </Text>
+        <Text>Here are some links to help you:</Text>
         <ul>
           <li>
-            <a href='https://github.com/internetdrew/resend-cse-home-challenge'>
+            <Link
+              href='https://github.com/internetdrew/resend-cse-home-challenge'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               View the tutorial repo
-            </a>
+            </Link>
           </li>
           <li>
-            <a href='https://andrewofnewyork.notion.site/Resend-CSE-Take-Home-Challenge-Customer-Tickets-187ffd770e1c80d8853cf05099f7cec1'>
+            <Link
+              href='https://andrewofnewyork.notion.site/Resend-CSE-Take-Home-Challenge-Customer-Tickets-187ffd770e1c80d8853cf05099f7cec1'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
               See my customer ticket responses
-            </a>
+            </Link>
           </li>
           <li>
-            <a href='https://www.loom.com/share/f3880b36f9e44b64babee286b6eca789?sid=d2046e9c-9ce5-4409-bbec-933f865d8455'>
-              See the Loom of a bug I found
-            </a>
+            <Link
+              href='https://www.loom.com/share/f3880b36f9e44b64babee286b6eca789?sid=d2046e9c-9ce5-4409-bbec-933f865d8455'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              See the Loom of a Resend bug I found
+            </Link>
           </li>
         </ul>
       </Body>
